f0f7d11ef23053c8d27720330a8e852dd411d15d
Kam Lagan <kam.lagan@aprexo.com>
2019-04-10 18:33:10 +0000

    added simple configuration samples will consolidate at later date

---
+++ Samples.Configuration/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace Samples.Configuration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new Dictionary<string, string>
+            {
+                ["HelloMessage"] = "Hello, World!",
+                ["GoodbyeMessage"] = "Goodbye, World!"
+            };
+
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(settings)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables("ConfigurationBasics.");
+
+            var config = builder.Build();
+
+            Console.WriteLine($"{config["HelloMessage"]}");
+            Console.WriteLine($"{config["GoodbyeMessage"]}");
+
+            Console.ReadKey();
+        }
+    }
+}
